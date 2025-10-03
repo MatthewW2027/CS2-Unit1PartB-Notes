54dd6218fdd42249761f2f87e8631f48decdfb3f
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-10-03 12:42:58 +0000

    1.8 notes

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+/**
+ * @author Matthew W.
+ * @since 3 Oct 2025 
+ * @version 1.0 
+ * Notes for topics 1.8-1.15 
+ */
 public class Main {
-
+   /**
+    * main METHOD is teh entry point to the program 
+    * @param INPUT is an array of Strings 
+    * @return VOID means NO OUTPUT 
+    */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
